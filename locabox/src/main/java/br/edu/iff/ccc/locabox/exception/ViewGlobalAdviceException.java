8c51d3ba1d5147cf3c93dc9528f15dbfcc06cba8
PoisON <poison.me1337@gmail.com>
2025-09-14 18:00:50 -0300

    Add global exception handler classes
    Introduced RESTGlobalAdviceException and ViewGlobalAdviceException classes for centralized exception handling. ViewGlobalAdviceException provides a default error handler for controller exceptions, returning an error view with exception details.

---
+++ locabox/src/main/java/br/edu/iff/ccc/locabox/exception/ViewGlobalAdviceException.java
@@ -0,0 +1,21 @@
+package br.edu.iff.ccc.locabox.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice(annotations = Controller.class)
+public class ViewGlobalAdviceException {
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("error");
+        return mav;
+    }
+
+}
